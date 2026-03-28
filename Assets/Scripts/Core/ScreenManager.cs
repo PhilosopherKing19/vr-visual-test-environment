8f64bfca7f3c3953c9b63897fcd4b3b73ab85d10
PhilosopherKing19 <ermal.balaj@student.uni-tuebingen.de>
2026-03-28 15:22:28 +0100

    Added grid screen generation with inspector layout selection

--- Assets/Scripts/Core/ScreenManager.cs
+++ Assets/Scripts/Core/ScreenManager.cs
@@ -1,7 +1,17 @@
 using UnityEngine;
+using static UnityEngine.Rendering.DebugUI.Table;
 
 public class ScreenManager : MonoBehaviour
 {
+    public enum LayoutType
+    {
+        Linear,
+        Grid
+    }
+
+    [SerializeField] private LayoutType layoutType;
+
+    // Linear layout settings
     [SerializeField] private GameObject screenPrefab;
     [SerializeField] private int screenCount = 2;
     [SerializeField] private float horizontalSpacing = 1.5f;
@@ -12,14 +22,25 @@ public class ScreenManager : MonoBehaviour
     [SerializeField] private GameObject screenContainerPrefab;
     [SerializeField] private bool centerLayout = true;
 
+    // Grid layout settings
+    [SerializeField] private float verticalSpacing;
+    [SerializeField] private int rowCount;
+    [SerializeField] private int columnCount;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        if(layoutType == LayoutType.Linear) GenerateLinearScreens();
+        else if(layoutType == LayoutType.Grid) GenerateGridScreens();
+    }
+
+    void GenerateLinearScreens()
+    {
+        float centerOffset = (screenCount - 1) / 2;
         for (int i = 1; i <= screenCount; i++)
         {   // Werte der Screens werden berrechnet
             float y = startPosition.y;
             float z = startPosition.z + i * depthSpacing;
-            float centerOffset = (screenCount - 1) / 2;
             float x = (i - centerOffset) * horizontalSpacing;
             Vector3 position = new Vector3(x, y, z);
 
@@ -29,7 +50,42 @@ public class ScreenManager : MonoBehaviour
 
             // muss in Quaternion umgewandelt werden, da unity Quaternion erwartet und nicht Vector3
             neuerScreen.transform.rotation = Quaternion.Euler(defaultRotation);
+        }
+    }
 
+
+    void GenerateGridScreens()
+    {
+        float columnCenterOffset = (columnCount - 1) / 2f;
+        float rowCenterOffset = (rowCount - 1) / 2f;
+
+        for(int row = 0; row < rowCount; row++){
+            for(int col = 0; col < columnCount; col++){
+
+                // x berrechnung
+                float xOffset = (col - columnCenterOffset) * horizontalSpacing;
+                float x = startPosition.x + xOffset;
+            
+                // y berrechnung
+                float yOffset = (rowCenterOffset - row) * verticalSpacing;
+                float y = startPosition.y + yOffset;
+
+                // z berrechnung
+                float z = startPosition.z;
+
+                Vector3 position = new Vector3(x, y, z);
+
+                // Screens werden erzeugt und Werte, werden übergeben
+                GameObject neuerScreen = Instantiate(screenPrefab);
+                
+                neuerScreen.transform.SetParent(screenContainer);
+
+                neuerScreen.transform.position = position;
+
+                // muss in Quaternion umgewandelt werden, da unity Quaternion erwartet und nicht Vector3
+                neuerScreen.transform.rotation = Quaternion.Euler(defaultRotation);
+
+            }
         }
     }
     // Update is called once per frame
